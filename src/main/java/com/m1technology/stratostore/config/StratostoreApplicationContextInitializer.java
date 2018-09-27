410503a14b0aad8d40d6da4718a6ef46ec7fd464
M1Brian <brian@m1technology.com>
2018-09-26 23:56:46 -0400

    code clean up

--- src/main/java/com/m1technology/stratostore/config/StratostoreApplicationContextInitializer.java
+++ src/main/java/com/m1technology/stratostore/config/StratostoreApplicationContextInitializer.java
@@ -6,8 +6,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StratostoreApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-//        ConfigurableEnvironment env =  configurableApplicationContext.getEnvironment();
-//        configurableApplicationContext.addBeanFactoryPostProcessor(new ReposBeanFactoryPostProcessor(env));
         configurableApplicationContext.addBeanFactoryPostProcessor(new ReposBeanFactoryPostProcessor());
     }
 }
